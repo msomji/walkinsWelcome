00388260c2976111513a2a340f06afe8fa9bb46e
Maisam Somji <maisam.somji@gmail.com>
2020-04-30 13:05:07 -0400

    able to see each other

--- client/src/Components/VideoCall/VideoCall.component.tsx
+++ client/src/Components/VideoCall/VideoCall.component.tsx
@@ -6,7 +6,7 @@ export interface VideoCallProps {
     token: string
 }
 
-const VideoCall: React.FC<VideoCallProps> = ({token}) => {
+const VideoCall: React.FC<VideoCallProps> = ({ token }) => {
 
     const connector = (token: string) => {
         connect(token).then(room => {
@@ -14,55 +14,58 @@ const VideoCall: React.FC<VideoCallProps> = ({token}) => {
             createLocalVideoTrack().then(track => {
                 const localMediaContainer = document.getElementById('local-media');
                 localMediaContainer!.appendChild(track.attach());
-              });
+            });
 
             const localParticipant = room.localParticipant;
             console.log(`Connected to the Room as LocalParticipant "${localParticipant}"`);
-            
+
             // Log any Participants already connected to the Room
             room.participants.forEach(participant => {
-              console.log(`Participant "${participant}" is connected to the Room`);
-              participant.tracks.forEach((publication:any) => {
-                if (publication.isSubscribed) {
-                  const track = publication.track;
-                  document.getElementById('remote-media-div')!.appendChild(track.attach());
-                }
-              });
-            
-              participant.on('trackSubscribed', (track:any) => {
-                document.getElementById('remote-media-div')!.appendChild(track.attach());
-              });
+                console.log(`Participant "${participant}" is connected to the Room`);
+                participant.tracks.forEach((publication: any) => {
+                    if (publication.isSubscribed) {
+                        const track = publication.track;
+                        document.getElementById('remote-media-div')!.appendChild(track.attach());
+                    }
+                });
+
+                participant.on('trackSubscribed', (track: any) => {
+                    document.getElementById('remote-media-div')!.appendChild(track.attach());
+                });
             });
-            
+
             // Log new Participants as they connect to the Room
             room.on('participantConnected', participant => {
                 console.log(`Participant "${participant.identity}" connected`);
-              
-                participant.tracks.forEach((publication:any) => {
-                  if (publication.isSubscribed) {
-                    const track = publication.track;
+
+                participant.tracks.forEach((publication: any) => {
+                    if (publication.isSubscribed) {
+                        const track = publication.track;
+                        document.getElementById('remote-media-div')!.appendChild(track.attach());
+                    }
+                });
+
+                participant.on('trackSubscribed', (track: any) => {
                     document.getElementById('remote-media-div')!.appendChild(track.attach());
-                  }
                 });
-              
-                participant.on('trackSubscribed', (track:any) => {
-                  document.getElementById('remote-media-div')!.appendChild(track.attach());
-                });
-              });
-            
-            // Log Participants as they disconnect from the Room
-            room.once('participantDisconnected', participant => {
-              console.log(`Participant "${participant}" has disconnected from the Room`);
             });
+
+
 
 
             room.on('participantConnected', participant => {
                 console.log(`Participant connected: ${participant.identity}`);
-              });
-              
-              room.on('participantDisconnected', participant => {
+            });
+
+            room.on('participantDisconnected', participant => {
                 console.log(`Participant disconnected: ${participant.identity}`);
-              });
+                // Detach the local media elements
+                room.localParticipant.tracks.forEach(publication => {
+                    const attachedElements = publication.track;
+                    console.log(attachedElements);
+                    // attachedElements.forEach((element:any) => element.remove());
+                });
+            });
 
 
 
@@ -74,9 +77,9 @@ const VideoCall: React.FC<VideoCallProps> = ({token}) => {
 
 
 
-          }, error => {
+        }, error => {
             console.error(`Unable to connect to Room: ${error.message}`);
-          });
+        });
     }
     useEffect(() => {
         connector(token);
